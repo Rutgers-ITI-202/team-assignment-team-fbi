31577b83d23f08c3592feb5b20cf9dcf5824cce3
dancoder123 <dantocz@gmail.com>
2017-02-23 00:43:30 -0500

    Update Room.java

--- src/adventure/Room.java
+++ src/adventure/Room.java
@@ -1,1 +1,28 @@
+public class Room {
+	private int number;
+	private String description;
+	private boolean hasPlayer;
+	private String[] stuff;
+
+	public Room(int n, String d){
+		this.number = n;
+		this.description = d;
+		this.hasPlayer = false;
+		this.stuff = new String[5];
+	}	
+
+	public int getNumber(){
+		return this.number;
+	}
+
+	public void showDescription(){
+		System.out.println(this.description);
+	}	
+
+	public void getstuff() {
+		for (String x : this.stuff)
+			System.out.print(x);
+	}
+
+}
 
