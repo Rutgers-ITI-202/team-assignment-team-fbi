57aa594ed8196a26fe65c1c05d98d553d17e5761
sumanth1294 <sumanth1294@gmail.com>
2017-02-24 14:35:55 -0500

    Update Player.java

--- src/adventure/Player.java
+++ src/adventure/Player.java
@@ -1,62 +1,65 @@
-/*
-An adventurer has:
-•	A location (some room).
-•	An inventory (the things being carried).  When the player executes the “take thing” command, the item should be added to the inventory.
-
-An adventurer can:
-•	Move from room to room.
-•	Carry a number of objects.
-•	Pick up, drop, look at, and use various objects.
-*/
-
+import java.util.*;
 public class Player {
 	
 	private int room; // Basically since room is just 1, so this will be current room of player
-	private String[] items; //Player's stack
+	private ArrayList<String> items; //Player's stack
 	private String name;
 
 	public Player() {
-		this.room = 0; // Indicating that he is room at zero index.	
-		this.items = new String[0];
+		this.room = -1; // Indicating that he is room at zero index.	
+		this.items = new ArrayList<String>(0);
 		this.name = "Rocky";
 	}
 
 	//Since the room variable is private, we need the get method
-	public int getroom(){ 
+	public int getRoom(){ 
 		return this.room;
 	}
 
-	public void setname(String x){
+	public void setRoom(int x){ 
+		this.room = x;
+	}
+
+	public void setName(String x){
 		this.name = x;
 	}
 
-	public String getname(){
+	public String getName(){
 		return this.name;
 	}
 
-
 	//This is to update the room in which the Player is.
-	public void movenext(){
+	public void moveNext(){
 		this.room +=1;
 	}
 
 	//This is to update the room in which the Player is.
-	public void moveback(){
+	public void moveBack(){
 		this.room -=1;
 	}
 
 	//this will print out the lists of Item in Player's Sack
-	public void getitems(){
-		for(String i : this.items) {
-			System.out.println(i);
+	public void getItems(){
+		if(this.items.isEmpty()) {
+			System.out.println("You do not have any item");
 		}
+		else {
+				for(String i : this.items) {
+					System.out.println("\n" + i);
+				}
+			}
 	}
 
-	public void additem(){
+	public boolean addItem(String item){
 		//this is where an item is added to the player's stash from the room
+		return this.items.add(item);
 	}
 
-	public void useitem(){
+	public boolean useItem(String item){
 		//this is where an item is used from the player's stash
+		return this.items.remove(item);
 	}
+
+
 }
+
