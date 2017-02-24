7c954cef0b316281ae4d7e1f954ea95eeef17d60
dancoder123 <dantocz@gmail.com>
2017-02-24 01:47:47 -0500

    Update TextAdventure.java

--- src/adventure/TextAdventure.java
+++ src/adventure/TextAdventure.java
@@ -5,13 +5,29 @@ This class:
 We will make an adventure game based around FBI.
 */
 
-package adventure;
+import java.util.*;
 
 public class TextAdventure {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static Scanner in = new Scanner(System.in);
 
+	// public String readInput(){
+	// 	try
+	//String input = in.
+	// 	}
+	// }
+
+
+	public static void main(String[] args) {
+		System.out.println("You are an FBI agent working in the city of Gotham.\n You have entered an apartment complex in the worst part of the city");
+		System.out.println("You enter the lobby, and an older man is sitting in the chair, and looks up at you.");
+		System.out.println("What should we call you?");
+		System.out.println("Please enter your name");
+		String playerName = in.nextLine();
+		
+
+	
+		
 	}
 
 }
