2b622b83fe4bb198c20ecd889c8e9921dd4b41f4
dancoder123 <dantocz@gmail.com>
2017-02-24 01:44:44 -0500

    Update Player.java

--- src/adventure/Player.java
+++ src/adventure/Player.java
@@ -10,18 +10,30 @@ An adventurer can:
 */
 
 public class Player {
+	
 	private int room; // Basically since room is just 1, so this will be current room of player
-	private String[] items;
+	private String[] items; //Player's stack
+	private String name;
 
 	public Player() {
 		this.room = 0; // Indicating that he is room at zero index.	
-		this.items = new String(0);
+		this.items = new String[0];
+		this.name = "Rocky";
 	}
 
 	//Since the room variable is private, we need the get method
 	public int getroom(){ 
 		return this.room;
 	}
+
+	public void setname(String x){
+		this.name = x;
+	}
+
+	public String getname(){
+		return this.name;
+	}
+
 
 	//This is to update the room in which the Player is.
 	public void movenext(){
@@ -33,12 +45,18 @@ public class Player {
 		this.room -=1;
 	}
 
-	//this will print out the lists of Item in the room
+	//this will print out the lists of Item in Player's Sack
 	public void getitems(){
 		for(String i : this.items) {
 			System.out.println(i);
 		}
 	}
+
+	public void additem(){
+		//this is where an item is added to the player's stash from the room
+	}
+
+	public void useitem(){
+		//this is where an item is used from the player's stash
+	}
 }
-
-
