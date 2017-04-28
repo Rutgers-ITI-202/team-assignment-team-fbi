be65e6b40506713a4f5773ca41144428ce04960e
dancoder123 <dantocz@gmail.com>
2017-04-28 18:05:21 -0400

    Delete TextAdventure.java

--- TextAdventure.java
+++
@@ -1,9 +0,0 @@
-package adventure;
-
-public class TextAdventure {
-    private static AdventureModel model = new AdventureModel();
-
-    public static void main(String[] args) {
-        model.playGame();
-    }
-}
